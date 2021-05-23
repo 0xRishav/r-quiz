3744b1eff97ff74548651d7a81248677f6d11960
Rishav Bharti <rshvbharti@gmail.com>
2021-05-23 21:53:04 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Option, Question } from "./data/quiz.types";
-import { quizOne } from "./data/data";
+// import { quizOne } from "./data/data";
 import Header from "./components/Header";
+import { Home, QuizPage, Result, SignIn, SignUp, Landing } from "./pages";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
+import { useQuiz } from "./contexts/quiz-context/QuizContext";
+import PrivateRoute from "./helpers/PrivateRoute";
+import { useAuth } from "./contexts/auth-context/authContext";
 
 function App() {
-  const [score, setScore] = useState(0);
-  const getScore = (
-    currentScore: number,
-    question: Question,
-    option: Option
-  ): number => {
+  const { isUserLoggedIn } = useAuth();
+  const {
+    state: { reactQuiz, cssQuiz, javascriptQuiz, nodejsQuiz },
+  } = useQuiz();
+  type getScorePropTypes = {
+    currentScore: number;
+    question: Question;
+    option: Option;
+  };
+
+  const getScore = ({
+    currentScore,
+    question,
+    option,
+  }: getScorePropTypes): number => {
     return option.isRight ? currentScore + question.points : currentScore;
   };
 
   return (
-    <div className="App">
-      <Header username="rishav" score={score} />
-      <button onClick={() => setScore(score + 1)}>Increase Score</button>
-      <button onClick={() => setScore(score - 1)}>Decrease Score</button>
+    <div className="font-Poppins App w-5/6 m-auto md:w-4/6 xl:w-3/6 2xl:w-2/6">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={isUserLoggedIn ? Home : Landing} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/react-quiz">
+            <QuizPage quiz={reactQuiz} />
+          </Route>
+          <Route path="/css-quiz">
+            <QuizPage quiz={cssQuiz} />
+          </Route>
+          <Route path="/nodejs-quiz">
+            <QuizPage quiz={nodejsQuiz} />
+          </Route>
+          <Route path="/javascript-quiz">
+            <QuizPage quiz={javascriptQuiz} />
+          </Route>
+          <Route path="/results" component={Result} />
+        </Switch>
+      </Router>
     </div>
   );
 }
